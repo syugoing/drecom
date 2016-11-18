394752d156259f4f983325c04a364c44651c51fd
syugoing <shugo.sawada.18@gmail.com>
2016-11-18 08:40:43 +0000

    comment

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,6 +1,9 @@
 class SharesController < ApplicationController
   def index
     @topics = Topic.all
+    @topic = Topic.new
     @missions = Mission.all
+    @comment = @topic.comments.build
+    @comments = Comment.all
   end
 end
